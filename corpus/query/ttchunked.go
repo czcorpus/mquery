341a2741ef62794c1174e99804e9e8983ba754fd
Tomas Machalek <tomas.machalek@gmail.com>
2023-09-07 15:41:11 +0200

    Streaming freqs - first prototype

--- corpus/query/ttchunked.go
+++ corpus/query/ttchunked.go
@@ -20,11 +20,16 @@ package query
 
 import (
 	"encoding/json"
+	"fmt"
 	"math/rand"
+	"mquery/corpus"
+	"mquery/rdb"
 	"mquery/results"
 	"net/http"
+	"sync"
 	"time"
 
+	"github.com/czcorpus/cnc-gokit/unireq"
 	"github.com/gin-gonic/gin"
 )
 
@@ -32,10 +37,11 @@ type StreamData struct {
 	Entries results.FreqDistrib `json:"entries"`
 	Count   int                 `json:"count"`
 	Total   int                 `json:"total"`
+	Error   string              `json:"error"`
 }
 
-// mockFreqCalculation is a testing replacement for (not yet available)
-// chunked text types freq. calculation
+// mockFreqCalculation is a testing replacement for
+// `streamCalc`
 func mockFreqCalculation() chan StreamData {
 	messageChannel := make(chan StreamData, 10)
 	go func() {
@@ -68,7 +74,11 @@ func mockFreqCalculation() chan StreamData {
 			counter++
 			newFreq := randItem()
 			ans.MergeWith(newFreq)
-			messageChannel <- StreamData{ans, counter, 30}
+			messageChannel <- StreamData{
+				Entries: ans,
+				Count:   counter,
+				Total:   30,
+			}
 			time.Sleep(1 * time.Second)
 			if counter >= 30 {
 				close(messageChannel)
@@ -79,32 +89,130 @@ func mockFreqCalculation() chan StreamData {
 	return messageChannel
 }
 
+func (a *Actions) streamCalc(query, attr, corpusID string, flimit, maxItems int) (chan StreamData, error) {
+	messageChannel := make(chan StreamData, 10)
+	corpusPath := a.conf.GetRegistryPath(corpusID)
+	sc, err := corpus.OpenSplitCorpus(a.conf.SplitCorporaDir, corpusPath)
+	if err != nil {
+		close(messageChannel)
+		return messageChannel, err
+	}
+
+	result := new(results.FreqDistrib)
+	result.Freqs = make([]*results.FreqDistribItem, 0)
+	mergedFreqLock := sync.Mutex{}
+
+	go func() {
+		wg := sync.WaitGroup{}
+		wg.Add(len(sc.Subcorpora))
+
+		for chunkIdx, subc := range sc.Subcorpora {
+			go func(chIdx int) {
+				defer wg.Done()
+				args, err := json.Marshal(rdb.FreqDistribArgs{
+					CorpusPath: corpusPath,
+					SubcPath:   subc,
+					Query:      query,
+					Crit:       fmt.Sprintf("%s 0", attr),
+					FreqLimit:  flimit,
+					MaxResults: maxItems,
+				})
+				if err != nil {
+					messageChannel <- StreamData{
+						Count: chunkIdx,
+						Total: len(sc.Subcorpora),
+						Error: err.Error(),
+					}
+					return
+				}
+
+				wait, err := a.radapter.PublishQuery(rdb.Query{
+					Func: "freqDistrib",
+					Args: args,
+				})
+				if err != nil {
+					messageChannel <- StreamData{
+						Count: chunkIdx,
+						Total: len(sc.Subcorpora),
+						Error: err.Error(),
+					}
+					return
+
+				} else {
+					tmp := <-wait
+					resultNext, err := rdb.DeserializeTextTypesResult(tmp)
+					if err != nil {
+						messageChannel <- StreamData{
+							Count: chIdx,
+							Total: len(sc.Subcorpora),
+							Error: err.Error(),
+						}
+						return
+					}
+					mergedFreqLock.Lock()
+					result.MergeWith(&resultNext)
+					mergedFreqLock.Unlock()
+					messageChannel <- StreamData{
+						Entries: *result,
+						Count:   chIdx,
+						Total:   len(sc.Subcorpora),
+						Error:   resultNext.Error,
+					}
+				}
+			}(chunkIdx)
+		}
+		wg.Wait()
+		close(messageChannel)
+	}()
+
+	return messageChannel, nil
+}
+
 type streamingError struct {
 	Error string `json:"error"`
 }
 
-func (a *Actions) TextTypesChunked(ctx *gin.Context) {
+func (a *Actions) writeStreamingError(ctx *gin.Context, err error) {
+	messageJSON, err2 := json.Marshal(streamingError{err.Error()})
+	if err2 != nil {
+		ctx.String(http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+	// We use status 200 here deliberately as we don't want to trigger
+	// the error handler.
+	ctx.String(http.StatusOK, string(messageJSON))
+}
+
+func (a *Actions) TextTypesStreamed(ctx *gin.Context) {
 	ctx.Writer.Header().Set("Content-Type", "text/event-stream")
 	ctx.Writer.Header().Set("Cache-Control", "no-cache")
 	ctx.Writer.Header().Set("Connection", "keep-alive")
 
 	defer ctx.Writer.Flush()
 
-	calc := mockFreqCalculation()
+	q := ctx.Request.URL.Query().Get("q")
+	attr := ctx.Request.URL.Query().Get("attr")
+	flimit, ok := unireq.GetURLIntArgOrFail(ctx, "flimit", 1)
+	if !ok {
+		return
+	}
+	maxItems, ok := unireq.GetURLIntArgOrFail(ctx, "maxItems", 0)
+	if !ok {
+		return
+	}
+
+	calc, err := a.streamCalc(q, attr, ctx.Param("corpusId"), flimit, maxItems)
+	if err != nil {
+		a.writeStreamingError(ctx, err)
+		return
+	}
 	for message := range calc {
 		messageJSON, err := json.Marshal(message)
 		if err == nil {
 			ctx.String(http.StatusOK, "data: %s\n\n", messageJSON)
 
 		} else {
-			// We use status 200 here deliberately as we don't want to trigger
-			// the error handler.
-			messageJSON, err2 := json.Marshal(streamingError{err.Error()})
-			if err2 != nil {
-				ctx.String(http.StatusInternalServerError, "Internal Server Error")
-				return
-			}
-			ctx.String(http.StatusOK, string(messageJSON))
+			a.writeStreamingError(ctx, err)
 			return
 		}
 		ctx.Writer.Flush()
