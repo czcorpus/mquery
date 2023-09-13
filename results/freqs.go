3b0e63a90e3b3bc8c64464bfdcc0259e405fc9e4
Tomas Machalek <tomas@machalek.xyz>
2023-09-13 18:25:23 +0200

    Fix ipm, norm recalculation

--- results/freqs.go
+++ results/freqs.go
@@ -133,12 +133,10 @@ func (res *FreqDistrib) MergeWith(other *FreqDistrib) {
 		if v1 != nil {
 			v1.CollWeight = 0 // we cannot merge coll values
 			v1.Freq += v2.Freq
-			v1.Norm = res.CorpusSize
-			v1.IPM = float32(v1.Freq) / float32(res.CorpusSize) * 1e6
+			v1.IPM = float32(v1.Freq) / float32(v1.Norm) * 1e6
 
 		} else {
-			v2.Norm = res.CorpusSize
-			v2.IPM = float32(v2.Freq) / float32(res.CorpusSize) * 1e6
+			// orig IPM should be OK for the first item so no need to set it here
 			res.Freqs = append(res.Freqs, v2)
 		}
 	}
