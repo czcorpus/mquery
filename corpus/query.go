e3a3c4ad38426ebaf7d7416a70b93082c2030ba9
Tomas Machalek <tomas@machalek.xyz>
2024-03-08 14:21:14 +0100

    Improve concordance query handler and related stuff

---
+++ corpus/query.go
@@ -0,0 +1,36 @@
+// Copyright 2024 Tomas Machalek <tomas.machalek@gmail.com>
+// Copyright 2024 Institute of the Czech National Corpus,
+//                Faculty of Arts, Charles University
+//   This file is part of MQUERY.
+//
+//  MQUERY is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  MQUERY is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with MQUERY.  If not, see <https://www.gnu.org/licenses/>.
+
+package corpus
+
+import (
+	"fmt"
+	"strings"
+)
+
+func SubcorpusToCQL(tt TextTypes) string {
+	var buff strings.Builder
+	for attr, values := range tt {
+		pAttr := strings.Split(attr, ".")
+		for _, value := range values {
+			buff.WriteString(
+				fmt.Sprintf(` within <%s %s="%s" />`, pAttr[0], pAttr[1], value))
+		}
+	}
+	return buff.String()
+}
