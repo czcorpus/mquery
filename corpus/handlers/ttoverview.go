0b222ab2460d199da28f68b2e348a767ccc30a62
Tomas Machalek <tomas@machalek.xyz>
2024-03-08 15:21:48 +0100

    Implement text-types-overview, refactor code ...
    ... related to corpus, query and subc determining

--- corpus/handlers/ttoverview.go
+++ corpus/handlers/ttoverview.go
@@ -20,12 +20,101 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"mquery/rdb"
+	"mquery/results"
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/czcorpus/cnc-gokit/uniresp"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
 )
 
-func (a *Actions) TextTypesOverview(ctx *gin.Context) {
+type ttOverviewResult map[string]results.FreqDistrib
 
-	// TODO missing implementation
-	// note: make several parallel queries for different attributes
-	// and merge them into a single result
+func (a *Actions) TextTypesOverview(ctx *gin.Context) {
+	queryProps := DetermineQueryProps(ctx, a.conf)
+	if queryProps.hasError() {
+		uniresp.RespondWithErrorJSON(ctx, queryProps.err, queryProps.status)
+		return
+	}
+
+	flimit := 1
+	if ctx.Request.URL.Query().Has("flimit") {
+		var err error
+		flimit, err = strconv.Atoi(ctx.Request.URL.Query().Get("flimit"))
+		if err != nil {
+			uniresp.WriteJSONErrorResponse(
+				ctx.Writer,
+				uniresp.NewActionErrorFrom(err),
+				http.StatusUnprocessableEntity,
+			)
+			return
+		}
+	}
+	corpusPath := a.conf.GetRegistryPath(queryProps.corpus)
+
+	mergedFreqLock := sync.Mutex{}
+	result := make(ttOverviewResult)
+	errs := make([]error, 0, len(queryProps.corpusConf.TTOverviewAttrs))
+	wg := sync.WaitGroup{}
+	wg.Add(len(queryProps.corpusConf.TTOverviewAttrs))
+
+	for _, attr := range queryProps.corpusConf.TTOverviewAttrs {
+		freqArgs := rdb.FreqDistribArgs{
+			CorpusPath:  corpusPath,
+			Query:       queryProps.query,
+			Crit:        fmt.Sprintf("%s 0", attr),
+			IsTextTypes: true,
+			FreqLimit:   flimit,
+		}
+
+		args, err := json.Marshal(freqArgs)
+		if err != nil {
+			uniresp.WriteJSONErrorResponse(
+				ctx.Writer,
+				uniresp.NewActionErrorFrom(err),
+				http.StatusInternalServerError,
+			)
+			return
+		}
+
+		wait, err := a.radapter.PublishQuery(rdb.Query{
+			Func: "freqDistrib",
+			Args: args,
+		})
+
+		if err != nil {
+			errs = append(errs, err)
+			log.Error().Err(err).Msg("failed to publish query")
+			wg.Done()
+
+		} else {
+			go func() {
+				defer wg.Done()
+				tmp := <-wait
+				resultNext, err := rdb.DeserializeTextTypesResult(tmp)
+				if err != nil {
+					errs = append(errs, err)
+					log.Error().Err(err).Msg("failed to deserialize query")
+				}
+				mergedFreqLock.Lock()
+				result[attr] = resultNext
+				mergedFreqLock.Unlock()
+			}()
+		}
+	}
+
+	wg.Wait()
+
+	if len(errs) > 0 {
+		uniresp.WriteJSONErrorResponse(
+			ctx.Writer, uniresp.NewActionErrorFrom(errs[0]), http.StatusInternalServerError)
+		return
+	}
+
+	uniresp.WriteJSONResponse(ctx.Writer, result)
 }
